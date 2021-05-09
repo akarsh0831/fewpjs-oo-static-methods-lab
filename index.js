9f1335b45401ef36578475e30dbf22a5eedc87e8
Akarsh Shukla <akarshshukla19@yahoo.com>
2021-05-09 17:25:12 +0000

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
+    static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    const avoid = /[#@!$*^({]/g
+    return string.replace(avoid, '')
+  }
+
+  static titleize(string) {
+    const avoid = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = []
+    let array = string.split(" ")
+    for (let i = 0; i < array.length; i++) {
+      if (i == 0) {
+        newString.push(this.capitalize(array[i]))
+      } else {
+        if (avoid.includes(array[i])) {
+          newString.push(array[i])
+        } else {
+          newString.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return newString.join(' ')
+  }
   //add static methods here
 }
